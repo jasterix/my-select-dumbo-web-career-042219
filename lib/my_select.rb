515d6b59690ff273116492a08be82c1ab9f8e838
Jasterix C <loune.calixte@gmail.com>
2019-04-09 20:05:42 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
 def my_select(collection)
  # your code here!
- def my_select(array)
   i = 0
   select = []
-  while i < array.length
-    if yield(array[i])
-      select << array[i]
+  while i < collection.length
+    if yield(collection[i])
+      select << collection[i]
     end
     i+=1
   end
